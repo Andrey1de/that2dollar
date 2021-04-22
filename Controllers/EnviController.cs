086e0a12194f2496cd5fe27281ff9502c4e560e2
Andrey1de <andrey1de@gmail.com>
2021-04-22 13:23:28 +0300

    EnviController

---
+++ Controllers/EnviController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace that2dollar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnviController : ControllerBase
+    {
+
+        // GET api/<EnvironmentController>/5
+        [HttpGet("")]
+        public IDictionary GeAllt()
+        {
+            return Environment.GetEnvironmentVariables();
+        }
+
+        [HttpGet("username")]
+        public string GetUserName()
+        {
+            return Environment.UserName;
+        }
+
+        [HttpGet("{name}")]
+        public string GetVariable(string name)
+        {
+            return Environment.GetEnvironmentVariable(name);
+        }
+        [HttpPost("{name}/{value}")]
+        public string SetVariable(string name, string value)
+        {
+            Environment.SetEnvironmentVariable(name, value);
+            return Environment.GetEnvironmentVariable(name);
+        }
+
+
+
+    }
+}
